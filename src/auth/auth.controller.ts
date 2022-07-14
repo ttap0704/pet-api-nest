36e7b821805a3789395c783e2a5c34ca71eeed65
LeeDaeGyu <ttap0704@naver.com>
2022-07-15 08:12:27 +0900

    set user permission

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,6 @@
-import { Body, Controller, Get, Ip, Post, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Ip, Post, Query, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { Users } from 'src/users/entities/users.entity';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt-auth.guard';
 
@@ -10,9 +12,17 @@ export class AuthController {
 
   @UseGuards(JwtAuthGuard)
   @Get('')
-  public async checkAuth(user: any) {
-    console.log(user);
-    return { check: true }
+  public async checkAuth(@Req() req: Request, @Query('type') type: string) {
+    const { user } = req;
+
+    let statusCode = 200;
+    if (![1, 2].includes(user['type']) && type == 'admin') {
+      statusCode = 403;
+    } else if (![0].includes(user['type']) && type == 'super') {
+      statusCode = 403;
+    }
+
+    return { statusCode }
   }
 
   @Post('/token')
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -14,7 +14,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    console.log(payload)
     return { id: payload.id, login_id: payload.login_id, nickname: payload.nickname, type: payload.type };
   }
 }
